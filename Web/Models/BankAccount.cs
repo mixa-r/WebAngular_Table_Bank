da04e9af1e16683754dbf886f47e9555b022ce61
mixa-r <53909744+mixa-r@users.noreply.github.com>
2021-06-14 20:18:29 +0500

    Добавьте файлы проекта.

---
+++ Web/Models/BankAccount.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Web.Models
+{
+    public class BankAccount
+    {
+        [Key]
+        public int BankAccountId { get; set; }
+
+        [Column(TypeName = "nvarchar(20)")]
+        [Required]
+        public string AccountNumber { get; set; }
+
+        [Column(TypeName = "nvarchar(100)")]
+        [Required]
+        public string AccountHolder { get; set; }
+
+        [Required]
+        public int BankId { get; set; }
+
+        [Required]
+        [Column(TypeName = "nvarchar(20)")]
+        public string IFSC { get; set; }
+    }
+}
